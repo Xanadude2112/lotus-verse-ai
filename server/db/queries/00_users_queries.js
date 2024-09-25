9376f6012c522064199e980f34c43ed38f07ca63
Tommy <tommy.azran@gmail.com>
2024-09-25 10:04:10 -0400

    Refactor users_queries.js to include additional user-related functions

--- server/db/queries/00_users_queries.js
+++ server/db/queries/00_users_queries.js
@@ -28,13 +28,97 @@ const getAllUsers = async () => {
     return allUsers.rows;
   } catch (err) {
     console.log(`ERROR: ${err}`);
-  } 
+  }
+};
+
+const getUserById = async (id) => {
+  try {
+    const user = await db.query(
+      `
+      SELECT * FROM users
+      WHERE id = $1;
+    `,
+      [id]
+    );
+    return user.rows[0];
+  } catch (err) {
+    console.log(`ERROR: ${err}`);
+  }
+};
+
+const getUserByEmail = async (email) => {
+  try {
+    const user = await db.query(
+      `
+      SELECT * FROM users
+      WHERE email = $1;
+    `,
+      [email]
+    );
+    return user.rows[0];
+  } catch (err) {
+    console.log(`ERROR: ${err}`);
+  }
+};
+
+const getUserByUsername = async (username) => {
+  try {
+    const user = await db.query(
+      `
+      SELECT * FROM users
+      WHERE username = $1;
+    `,
+      [username]
+    );
+    return user.rows[0];
+  } catch (err) {
+    console.log(`ERROR: ${err}`);
+  }
+};
 
 // UPDATE
 
+const udpdateUser = async (id, username, email, password) => {
+  try {
+    const updatedUser = await db.query(
+      `
+      UPDATE users
+      SET username = $2, email = $3, password = $4
+      WHERE id = $1
+      RETURNING *;
+    `,
+      [id, username, email, password]
+    );
+    return updatedUser.rows[0];
+  } catch (err) {
+    console.log(`ERROR: ${err}`);
+  }
+};
+
 // DELETE
 
+const deleteUser = async (id) => {
+  try {
+    const deletedUser = await db.query(
+      `
+      DELETE FROM users
+      WHERE id = $1
+      RETURNING *;
+    `,
+      [id]
+    );
+    return deletedUser.rows[0];
+  } catch (err) {
+    console.log(`ERROR: ${err}`);
+  }
+};
 
 module.exports = {
   createUser,
+  getAllUsers,
+  getUserById,
+  getUserByEmail,
+  getUserByUsername,
+  udpdateUser,
+  deleteUser,
 };
