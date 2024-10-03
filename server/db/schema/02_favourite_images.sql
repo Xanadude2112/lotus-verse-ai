03e98cfde517eb1d8c5e9309dfa225d06e574572
Tommy <tommy.azran@gmail.com>
2024-10-03 18:26:45 -0400

    register and login tests are successful

--- server/db/schema/02_favourite_images.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS favourite_images CASCADE;
-
-CREATE TABLE favourite_images (
-  id SERIAL PRIMARY KEY,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  image_id INTEGER REFERENCES images(id) ON DELETE CASCADE
-);
--- server/db/schema/05_post_comments.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS post_comments CASCADE;
-
-CREATE TABLE post_comments (
-  id SERIAL PRIMARY KEY,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE,
-  comment_content TEXT,
-  created_at TIMESTAMP DEFAULT NOW()
-);
